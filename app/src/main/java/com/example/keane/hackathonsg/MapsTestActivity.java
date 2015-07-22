fc0fb91e01650c1f916ee1b15271edfa04196fd3
Adriel AD Tan <ad55tan@gmail.com>
2015-07-22 20:55:54 +0800

    Google Map activity; doesn't cause crash, but haven't tested if it works

---
+++ app/src/main/java/com/example/keane/hackathonsg/MapsTestActivity.java
@@ -0,0 +1,31 @@
+package com.example.keane.hackathonsg;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsTestActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps_test);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        // Add a marker in Singapore, and move the camera.
+        LatLng singapore = new LatLng(1.3, 103.8);
+        map.addMarker(new MarkerOptions().position(singapore).title("Marker in Sydney"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(singapore));//, 10);
+    }
+}
