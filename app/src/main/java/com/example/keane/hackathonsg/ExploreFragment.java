56cc253aa0252a9ccf9e2ab99ac027ec06b40867
Adriel AD Tan <ad55tan@gmail.com>
2015-07-25 18:10:38 +0800

    Added Explore Design (half done)

--- app/src/main/java/com/example/keane/hackathonsg/ExploreFragment.java
+++ app/src/main/java/com/example/keane/hackathonsg/ExploreFragment.java
@@ -14,6 +14,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.Arrays;
 import java.util.List;
@@ -37,7 +38,9 @@ public class ExploreFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_explore, container, false);
+        View view = inflater.inflate(R.layout.fragment_explore, container, false);
+
+        return view;
     }
 
     @Override
