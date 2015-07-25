1cf7e0a0e3c47fddaacd9238d81910e3187b4314
keanekwa <keanekwa@live.com.sg>
2015-07-26 01:34:07 +0800

    Edited Navigation Drawer Design

--- app/src/main/java/com/example/keane/hackathonsg/MainActivity.java
+++ app/src/main/java/com/example/keane/hackathonsg/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.keane.hackathonsg;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -98,6 +99,7 @@ public class MainActivity extends ActionBarActivity
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
         actionBar.setDisplayShowTitleEnabled(true);
         actionBar.setTitle(mTitle);
+        actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.blue)));
     }
 
 
