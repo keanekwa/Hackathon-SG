7d6c9c4e0045f5c68825e037f246861fcdf88a5d
keanekwa <keanekwa@live.com.sg>
2015-07-25 23:29:31 +0800

    1128 friend search done

--- app/src/main/java/com/example/keane/hackathonsg/SearchFragment.java
+++ app/src/main/java/com/example/keane/hackathonsg/SearchFragment.java
@@ -4,33 +4,36 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
-import com.parse.GetDataCallback;
 import com.parse.ParseException;
-import com.parse.ParseImageView;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
-import com.parse.ParseUser;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class SearchFragment extends Fragment {
-    ParseUser currentUser = ParseUser.getCurrentUser();
-    String usernameText;
-    String jioText;
-    ListView friendsList;
-    ParseImageView profilePic;
-    TextView usernameTextView;
-    TextView jioTextView;
-    ArrayList<ParseUser>friendsArray = new ArrayList<>();
+    public static ArrayList<ParseObject> mRESULTS;
+    ListView mListView;
+    EditText searchText;
+    private ProgressBar loading;
+    ImageButton confirmButton;
+    ImageButton backButton;
+    String searchQuery;
+    RelativeLayout noResults;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -45,55 +48,66 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
        final View view = inflater.inflate(R.layout.fragment_profile, container, false);
-        jioText = "You have been jioed " + currentUser.getInt("noOfJios") + " times.";
-        usernameText = currentUser.getString("username");
-        usernameTextView = (TextView) view.findViewById(R.id.usernameText);
-        jioTextView = (TextView)view.findViewById(R.id.noOfJiosText);
-        usernameTextView.setText(usernameText);
-        jioTextView.setText(jioText);
-        profilePic = (ParseImageView)view.findViewById(R.id.profileImageView);
-        if(currentUser.getParseFile("profilePic")==null){
-            profilePic.setImageDrawable(getResources().getDrawable(R.drawable.defaultuserimage));
+        if(mRESULTS ==null) {
+            mRESULTS = new ArrayList<>();
         }
-        else{
-            profilePic.setParseFile(currentUser.getParseFile("profilePic"));
-            profilePic.setPlaceholder(getResources().getDrawable(R.drawable.defaultuserimage));
-            profilePic.loadInBackground(new GetDataCallback() {
-                @Override
-                public void done(byte[] bytes, ParseException e) {
-                }
-            });
-        }
-        ParseQuery<ParseUser> query = ParseUser.getQuery();
-        query.addDescendingOrder("username");
-        query.findInBackground(new FindCallback<ParseUser>() {
-            @Override
-            public void done(List<ParseUser> parseObjects, ParseException e) {
-                if (e == null) {
-                    friendsArray.clear();
-                    List<String> parseFriends = currentUser.getList("friendsList");
-                    for (int i = 0; i < parseFriends.size(); i++) {
-                        for (int j = 0; j < parseObjects.size(); j++) {
-                            if (parseFriends.get(i).toString().equals(parseObjects.get(j).getString("username"))) {
-                                friendsArray.add(parseObjects.get(j));
-                                break;
-                            }
-                            else if (j == parseObjects.size()){
-                                break;
-                            }
-                        }
-                    }
-                    friendsList = (ListView)view.findViewById(R.id.friendsListView);
-                    PhotosAdapter adapter = new PhotosAdapter(getActivity(), R.layout.friends_list_adapter, friendsArray);
-                    friendsList.setAdapter(adapter);
+        mListView = (ListView) view.findViewById(R.id.searchListView);
+        loading = (ProgressBar) view.findViewById(R.id.progressBar3);
+        loading.setVisibility(View.GONE);
+        searchText = (EditText)view.findViewById(R.id.searchText);
+        confirmButton = (ImageButton)view.findViewById(R.id.confirm_search);
+        backButton = (ImageButton)view.findViewById(R.id.backArrow);
+        noResults = (RelativeLayout)view.findViewById(R.id.noResults);
 
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mRESULTS.clear();
+                searchQuery = searchText.getText().toString().toLowerCase();
+                if (searchQuery.equals("")) {
+                    Toast.makeText(getActivity(), "Please enter text.", Toast.LENGTH_LONG).show();
+                } else {
+                    searchText.setText("");
+                    mRESULTS.clear();
                 }
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
             }
         });
 
         return view;
     }
 
+    public void search(){
+        loading.setVisibility(View.VISIBLE);
+        noResults.setVisibility(View.INVISIBLE);
+        final ParseQuery<ParseObject> query = ParseQuery.getQuery("onNationalDay");
+        query.addDescendingOrder("createdAt");
+        query.whereContains("postTitle", searchQuery);
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> parseObjects, ParseException e) {
+                if (e == null && parseObjects.size() > 0) {
+                    for (int j = 0; j < parseObjects.size(); j++) {
+                        mRESULTS.add(parseObjects.get(j));
+                    }
+                    loading.setVisibility(View.VISIBLE);
+                    EventsAdaptor adapter = new EventsAdaptor(getActivity(), R.layout.explore_list, mRESULTS, "Arts");
+                    mListView.setVisibility(View.VISIBLE);
+                    mListView.setAdapter(adapter);
+                    loading.setVisibility(View.GONE);
+                } else {
+                    noResults.setVisibility(View.VISIBLE);
+                    loading.setVisibility(View.INVISIBLE);
+                    query.cancel();
+                }
+            }
+        });
+    }
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
@@ -104,40 +118,53 @@ public class SearchFragment extends Fragment {
         super.onDetach();
     }
 
-    private class PhotosAdapter extends ArrayAdapter<ParseUser> {
-        //creating variables
+    private class EventsAdaptor extends ArrayAdapter<ParseObject>{
         private int mResource;
-        private ArrayList<ParseUser> mFriends;
+        private ArrayList<ParseObject> mEventsList;
 
-        public PhotosAdapter(Context context, int resource, ArrayList<ParseUser> friendsList) {
-            super(context, resource, friendsList);
-            mResource = resource;
-            mFriends = friendsList;
+        public EventsAdaptor(Context context, int res, ArrayList<ParseObject> list, String category){
+            super(context, res, list);
+            mResource = res;
+            mEventsList = list;
+//            mCategory = category;
         }
 
-        //display subject data in every row of listView
         @Override
-        public View getView(final int position, View row, ViewGroup parent) {
-            if (row == null) {
+        public View getView(final int pos, View row, ViewGroup parent){
+            if(row == null){
                 row = LayoutInflater.from(getContext()).inflate(mResource, parent, false);
             }
 
-            final ParseObject currentTopImage = mFriends.get(position);
-            TextView titleTextView = (TextView) row.findViewById(R.id.friendUsernameTextView);
-            titleTextView.setText(currentTopImage.getString("username"));
-            TextView subtitleTextView = (TextView) row.findViewById(R.id.friendJioText);
-            subtitleTextView.setText("Has been jioed " + currentTopImage.getInt("noOfJios") + " times.");
+            final ParseObject event = mEventsList.get(pos);
 
-            //set like button status on create
-            ParseImageView likeImageView = (ParseImageView) row.findViewById(R.id.friendImageView);
-            likeImageView.setParseFile(currentTopImage.getParseFile("profilePic"));
-            likeImageView.setPlaceholder(getResources().getDrawable(R.drawable.defaultuserimage));
-            likeImageView.loadInBackground(new GetDataCallback() {
-                @Override
-                public void done(byte[] bytes, ParseException e) {
+            TextView titleTv = (TextView)row.findViewById(R.id.exploreTitle);
+            TextView catTv = (TextView)row.findViewById(R.id.exploreCategory);
+            TextView locationTv = (TextView)row.findViewById(R.id.exploreLocation);
+            TextView orgTv = (TextView)row.findViewById(R.id.exploreOrganiser);
+            TextView dateTv = (TextView)row.findViewById(R.id.exploreDate);
 
-                }
-            });
+            titleTv.setText(event.getString("Title"));
+//            catTv.setText(Html.fromHtml("<b>Category:</b> " + mCategory + " - " + event.getString("Genre")));
+
+
+            String locText = "";
+            if(event.getString("Block")!=null) locText += (event.getString("Block") + " ");
+            if(event.getString("Street")!=null) locText += (event.getString("Street") + " ");
+            if(event.getString("BuildingName")!=null) locText += (event.getString("BuildingName") + " ");
+            if(event.getString("Floor")!=null){
+                if(event.getString("UnitNumber")!=null) locText += ("#" +event.getString("Floor")+ "-"+ event.getString("UnitNumber"));
+                else locText += ("Level " + event.getString("Floor"));
+            }
+            locationTv.setText(Html.fromHtml("<b>Location:</b> " + locText));
+
+            if(event.getString("Organiser")!=null) orgTv.setText(Html.fromHtml("<b>Organiser:</b> " + event.getString("Organiser")));
+            else orgTv.setVisibility(View.GONE);
+
+            if(event.getString("Date")!=null && event.getString("Date2")!=null){
+                if(event.getString("Date").equals(event.getString("Date2"))) dateTv.setText(Html.fromHtml("<b>Date:</b> " + event.getString("Date")));
+                else dateTv.setText(Html.fromHtml("<b>Date:</b> " + event.getString(" Date ") + " - " + event.getString("Date2")));
+            }
+
             return row;
         }
     }
