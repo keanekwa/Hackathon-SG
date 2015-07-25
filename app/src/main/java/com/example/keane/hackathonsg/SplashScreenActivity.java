a0f9b102f469d788671a02d1682903b2bf8027c4
keanekwa <keanekwa@live.com.sg>
2015-07-26 03:34:55 +0800

    0334 minor additions

--- app/src/main/java/com/example/keane/hackathonsg/SplashScreenActivity.java
+++ app/src/main/java/com/example/keane/hackathonsg/SplashScreenActivity.java
@@ -31,6 +31,12 @@ public class SplashScreenActivity extends ActionBarActivity {
                 ExploreFragment.allEvents.addAll(ExploreFragment.artsEvents);
                 for (int i = 0; i < ExploreFragment.artsEvents.size(); i++) {
                     ExploreFragment.artsEvents.get(i).put("Category", "Arts");
+                    if (ExploreFragment.artsEvents.get(i).get("rating")==null){
+                        ExploreFragment.artsEvents.get(i).put("rating", 0);
+                    }
+                    if (ExploreFragment.artsEvents.get(i).get("noOfRaters")==null){
+                        ExploreFragment.artsEvents.get(i).put("noOfRaters", 0);
+                    }
                 }
                 ParseQuery<ParseObject> query2 = new ParseQuery<>("artsEvents");
                 query2.addAscendingOrder("Date");
