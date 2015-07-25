b307672826a04793bdb61eaadfdb48486e439fe4
keanekwa <keanekwa@live.com.sg>
2015-07-26 02:19:34 +0800

    0219 explorefragment now works

--- app/src/main/java/com/example/keane/hackathonsg/SplashScreenActivity.java
+++ app/src/main/java/com/example/keane/hackathonsg/SplashScreenActivity.java
@@ -22,20 +22,45 @@ public class SplashScreenActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         ParseQuery<ParseObject> query = new ParseQuery<>("artsEvents");
+        query.addAscendingOrder("Date");
         query.findInBackground(new FindCallback<ParseObject>() {
             @Override
             public void done(List<ParseObject> list, ParseException e) {
                 ExploreFragment.artsEvents = new ArrayList<>(list);
-                ExploreFragment.allEvents =  new ArrayList<>();
+                ExploreFragment.allEvents = new ArrayList<>();
                 ExploreFragment.allEvents.addAll(ExploreFragment.artsEvents);
                 for (int i = 0; i < ExploreFragment.artsEvents.size(); i++) {
                     ExploreFragment.artsEvents.get(i).put("Category", "Arts");
                 }
-                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
-                startActivity(intent);
+                ParseQuery<ParseObject> query2 = new ParseQuery<>("artsEvents");
+                query2.addAscendingOrder("Date");
+                query2.setLimit(20);
+                query2.findInBackground(new FindCallback<ParseObject>() {
+                    @Override
+                    public void done(List<ParseObject> list2, ParseException e) {
+                        ExploreFragment.artsEvents2 = new ArrayList<>(list2);
+                        ExploreFragment.allEvents2 = new ArrayList<>();
+                        ExploreFragment.allEvents2.addAll(ExploreFragment.artsEvents2);
+                        ParseQuery<ParseObject> query3 = new ParseQuery<>("artsEvents");
+                        query3.addDescendingOrder("Date");
+                        query3.setLimit(20);
+                        query3.findInBackground(new FindCallback<ParseObject>() {
+                            @Override
+                            public void done(List<ParseObject> list3, ParseException e) {
+                                ExploreFragment.artsEvents3 = new ArrayList<>(list3);
+                                ExploreFragment.allEvents3 = new ArrayList<>();
+                                ExploreFragment.allEvents3.addAll(ExploreFragment.artsEvents3);
+                                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                                startActivity(intent);
+                            }
+                        });
+                    }
+                });
             }
-        });
-    }
+            });
+
+}
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
