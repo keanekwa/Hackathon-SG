42d0c8b3c77bd05f78d7005b192ecb05cba0d9d4
keanekwa <keanekwa@live.com.sg>
2015-07-25 19:54:10 +0800

    Fixed stupid color errors

--- app/src/main/java/com/example/keane/hackathonsg/ExploreFragment.java
+++ app/src/main/java/com/example/keane/hackathonsg/ExploreFragment.java
@@ -1,12 +1,10 @@
 package com.example.keane.hackathonsg;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -15,13 +13,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.parse.ParseObject;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
