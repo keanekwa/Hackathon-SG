7c1a62df3e418b4c0240ca1843ff2dcd637ec003
keanekwa <keanekwa@live.com.sg>
2015-07-26 08:10:38 +0800

    0810

--- app/src/main/java/com/example/keane/hackathonsg/SplashScreenActivity.java
+++ app/src/main/java/com/example/keane/hackathonsg/SplashScreenActivity.java
@@ -31,9 +31,11 @@ public class SplashScreenActivity extends ActionBarActivity {
                 ExploreFragment.allEvents = new ArrayList<>();
                 ExploreFragment.allEvents.addAll(ExploreFragment.artsEvents);
                 for (int i = 0; i < ExploreFragment.artsEvents.size(); i++) {
-                    ExploreFragment.artsEvents.get(i).put("Category", "Arts");
                     if (ExploreFragment.artsEvents.get(i).get("Rating")==null){
                         ExploreFragment.artsEvents.get(i).put("Rating", "General");
+                    }
+                    if (ExploreFragment.artsEvents.get(i).get("Category")==null){
+                        ExploreFragment.artsEvents.get(i).put("Category", "Arts");
                     }
                 }
                 ParseQuery<ParseObject> query2 = new ParseQuery<>("artsEvents");
@@ -52,8 +54,17 @@ public class SplashScreenActivity extends ActionBarActivity {
                             @Override
                             public void done(List<ParseObject> list3, ParseException e) {
                                 ExploreFragment.artsEvents3 = new ArrayList<>(list3);
+                                for (int i = 0; i < ExploreFragment.artsEvents3.size(); i++) {
+                                    if (ExploreFragment.artsEvents3.get(i).get("Rating")==null){
+                                        ExploreFragment.artsEvents3.get(i).put("Rating", "General");
+                                    }
+                                    if (ExploreFragment.artsEvents3.get(i).get("Category")==null){
+                                        ExploreFragment.artsEvents3.get(i).put("Category", "Arts");
+                                    }
+                                }
                                 ExploreFragment.allEvents3 = new ArrayList<>();
                                 ExploreFragment.allEvents3.addAll(ExploreFragment.artsEvents3);
+
                                 Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
                                 startActivity(intent);
                             }
